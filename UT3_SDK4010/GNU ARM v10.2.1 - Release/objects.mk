09d2160746e1a0499fe4466f643de9fa95a0297c
Jenish Rudani <jrudani1999@gmail.com>
2022-06-22 20:50:04 -0700

    Added UT-3 Scanner Application
    Only Scanning machine is present in this fw

--- UT3_SDK4010/GNU ARM v10.2.1 - Release/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS := C:/Users/kishore.valaboju/SimplicityStudio/SDKs/gecko-sdk//protocol/bluetooth/lib/EFR32BG22/GCC/libbluetooth.a C:/Users/kishore.valaboju/SimplicityStudio/SDKs/gecko-sdk//platform/radio/rail_lib/autogen/librail_release/librail_efr32xg22_gcc_release.a C:/Users/kishore.valaboju/SimplicityStudio/SDKs/gecko-sdk//platform/emdrv/nvm3/lib/libnvm3_CM33_gcc.a C:/Users/kishore.valaboju/SimplicityStudio/SDKs/gecko-sdk//protocol/bluetooth/lib/EFR32BG22/GCC/binapploader.o
-
-LIBS := -lgcc -lc -lm -lnosys
-
