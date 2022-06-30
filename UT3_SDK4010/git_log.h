50b4728cebfe1e1392e74eb23350a4d2fc7e7ad1
Jenish Rudani <jenish.rudani@guardrfid.com>
2022-06-29 17:43:37 -0700

    removed

--- UT3_SDK4010/git_log.h
+++
@@ -1,5 +0,0 @@
-//! =============== Auto-generated file, do not edit. =============== 
- 
-#define GIT_LINE_1 "4a6f33f (HEAD -> dev_master, origin/dev_master, origin/HEAD) 2022-06-09 Thu Jun 9 15:26" 
-#define GIT_LINE_2 "Merge remote-tracking branch 'origin/alex_wip_arch' into dev_master                                 " 
-#define GIT_URL "https://git-codecommit.us-west-2.amazonaws.com/v1/repos/UT-3" 
